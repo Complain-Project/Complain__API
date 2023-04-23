05a7a45bec6dc42adb3278f3c3e8f3824a01e6b7
Anh Tu <tula@zent.vn>
2023-04-23 14:00:57 +0700

    complain

---
+++ app/Http/Controllers/Admins/ComplainController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Admins;
+
+use App\Http\Controllers\Controller;
+use App\Services\Admins\ComplainService;
+use App\Traits\ResponseTrait;
+use Illuminate\Http\Request;
+
+class ComplainController extends Controller
+{
+    private ComplainService $complainService;
+
+    public function __construct(ComplainService $complainService)
+    {
+        $this->complainService = $complainService;
+    }
+
+    public function index(Request $request){
+        $complains = $this->complainService->index($request);
+
+        return $complains ? ResponseTrait::responseSuccess($complains) : ResponseTrait::responseError();
+    }
+
+    public function show($id){
+        $complain = $this->complainService->show($id);
+
+        return $complain ? ResponseTrait::responseSuccess($complain) : ResponseTrait::responseError();
+    }
+    public function reply(Request $request, $id){
+        $complain = $this->complainService->reply($request, $id);
+
+        return $complain ? ResponseTrait::responseSuccess() : ResponseTrait::responseError();
+    }
+
+    public function getAllDistrict()
+    {
+        $districts = $this->complainService->getAllDistrict();
+
+        return $districts ? ResponseTrait::responseSuccess($districts) : ResponseTrait::responseError();
+    }
+}
